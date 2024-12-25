b1d041933fdbfad70e6fcf2ea081dee7ca901de2
TheFreinBoy <tetievskiyvlad@gmail.com>
2024-12-25 23:30:13 +0200

    ⠀⠀⠀⠀

---
+++ Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть текст: ");
+        string input = Console.ReadLine();
+
+        string pattern = @"\{(?:[^{}]*|\{.*?\})*\}";
+  
+        string result = Regex.Replace(input, pattern, "{o}");
+
+        Console.Write("Результат: ");
+        Console.WriteLine(result);
+    }
+}
+//abc
